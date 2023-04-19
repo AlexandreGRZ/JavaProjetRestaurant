aa2e3bfa6a006eafe21ceb0c70b711db483c2940
AlexandreGrzegorczyk <alexandre.grzegorczyk@student.hepl.be>
2023-04-19 12:38:39 +0200

    :tada: INITIAL COMMIT :tada:

--- src/InterfaceGraphique/MainPage.java
+++ src/InterfaceGraphique/MainPage.java
@@ -3,35 +3,25 @@ package InterfaceGraphique;
 import javax.swing.*;
 import java.awt.*;
 
-public class MainPage extends JFrame{
-
-
-    private JPanel MainPanel;
+public class MainPage extends JFrame  {
     private JTable table1;
-    private JPanel AJOUTERAUMENU;
-    private JComboBox comboBox1;
-    private JComboBox comboBox2;
-    private JButton ajouterButton;
-    private JButton ajouterButton1;
-    private JPanel CréationDePlat;
-    private JPanel VueReservation;
+    private JButton AJOUTERAUMENUButton;
+    private JPanel panel1;
     private JTable table2;
-    private JTextField textField1;
-    private JTextField textField2;
-    private JComboBox comboBox3;
-    private JButton ajouterButton2;
-    private JTable table3;
-    private JTextField textField3;
-    private JButton voirButton;
+    private JButton AJOUTERPLATButton;
 
-    public MainPage(String Nom)
-    {
 
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+
+
+    public MainPage(String Nom) {
         super(Nom);
 
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        this.setContentPane(MainPanel);
+        this.setContentPane(panel1);
 
         this.pack();
     }
@@ -39,7 +29,7 @@ public class MainPage extends JFrame{
     public static void main(String[] args) {
         JFrame Init = new MainPage("MainPage");
 
-        Init.setSize(new Dimension(720, 500));
+        Init.setSize(new Dimension(1080, 720));
 
         Init.setVisible(true);
     }
